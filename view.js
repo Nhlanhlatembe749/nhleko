637a397f590bad0fc507fc88ce23115028901977
Nhlanhlatembe749 <nhlanhlatembe749@gmail.com>
2025-09-11 20:07:45 +0200

    Add files via upload
    this is a personal website sample

---
+++ view.js
@@ -0,0 +1,18 @@
+function change1(){
+    document.getElementById("hi").innerHTML="My Name is Nhlanhla Tembe \n aged 24 years old \n this is my first webpage currently residingat durban north for more info contact: 072 834 1184 / nhlanhlatembe749@gmail.com";
+
+
+
+}
+function change2(){
+    document.getElementById("hii").innerHTML="i enjoy programming approximately 40% of my day dedicated to programming and web development  "
+}
+
+function change3(){
+    document.getElementById("hiii").innerHTML="game developer  data science    web developer  software enginnering  cybersecurity "
+}
+
+function check(){
+     alert("submitted successful")
+     
+}
